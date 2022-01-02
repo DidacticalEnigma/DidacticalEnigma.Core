f66684209af4205d4400c7f154481cdc2460ed57
milleniumbug <milleniumbug@protonmail.com>
2022-01-02 15:25:26 +0100

    Runtime adding and removal of data sources

--- DidacticalEnigma.Core/Models/DataSources/DataSourceDispatcher.cs
+++ DidacticalEnigma.Core/Models/DataSources/DataSourceDispatcher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -14,21 +15,49 @@ namespace DidacticalEnigma.Core.Models.DataSources
         private readonly Dictionary<string, IDataSource> dataSources;
 
         private readonly Dictionary<string, DataSourceDescriptor> descriptors;
+        
+        private readonly Dictionary<Guid, string> removalIdentifiers;
 
-        public DataSourceDispatcher(IEnumerable<IDataSource> dataSources)
+        public DataSourceDispatcher()
         {
             this.dataSources = new Dictionary<string, IDataSource>();
             this.descriptors = new Dictionary<string, DataSourceDescriptor>();
+            this.removalIdentifiers = new Dictionary<Guid, string>();
+        }
+
+        public DataSourceDispatcher(IEnumerable<IDataSource> dataSources)
+            : this()
+        {
             foreach (var dataSource in dataSources)
             {
-                if (dataSource.GetType().GetProperty("Descriptor", BindingFlags.Static | BindingFlags.Public)?.GetValue(null) is
-                    DataSourceDescriptor descriptor)
-                {
-                    var dataSourceInstanceIdentifier = dataSource.InstanceIdentifier;
-                    var dataSourceIdentifier = descriptor.Guid.ToString() + (dataSourceInstanceIdentifier != null ? "|" + dataSourceInstanceIdentifier : "");
-                    this.dataSources.Add(dataSourceIdentifier, dataSource);
-                    this.descriptors.Add(dataSourceIdentifier, descriptor);
-                }
+                Add(dataSource);
+            }
+        }
+
+        public Guid Add(IDataSource dataSource)
+        {
+            if (dataSource.GetType().GetProperty("Descriptor", BindingFlags.Static | BindingFlags.Public)?.GetValue(null) is
+                DataSourceDescriptor descriptor)
+            {
+                var dataSourceInstanceIdentifier = dataSource.InstanceIdentifier;
+                var dataSourceIdentifier = descriptor.Guid.ToString() + (dataSourceInstanceIdentifier != null ? "|" + dataSourceInstanceIdentifier : "");
+                this.dataSources.Add(dataSourceIdentifier, dataSource);
+                this.descriptors.Add(dataSourceIdentifier, descriptor);
+                
+                var removalIdentifier = Guid.NewGuid();
+                this.removalIdentifiers[removalIdentifier] = dataSourceIdentifier;
+                return removalIdentifier;
+            }
+
+            throw new ArgumentException("The data source is invalid as it has no descriptor", nameof(dataSource));
+        }
+
+        public void Remove(Guid removalIdentifer)
+        {
+            if (this.removalIdentifiers.TryGetValue(removalIdentifer, out var dataSourceInstanceIdentifier))
+            {
+                this.dataSources.Remove(dataSourceInstanceIdentifier);
+                this.descriptors.Remove(dataSourceInstanceIdentifier);
             }
         }
 
